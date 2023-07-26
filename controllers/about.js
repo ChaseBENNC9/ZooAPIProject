1d49ca4925ab3e2ef127cc4a13d690bbcd23532e
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-07-26 21:28:11 +1200

    Created about route and controller

---
+++ controllers/about.js
@@ -0,0 +1,7 @@
+// Create a GET route
+const get = (req, res) => {
+    res.send("My Learner ID is 1000106289, my first name is Chase and my last name is Bennett-Hill. One thing in IT that I enjoy is Programming.");
+};
+
+// Export the get function
+export { get };
