60dfb4e012bfbf886771a87b2bc07984300b5ff4
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-07-26 21:29:17 +1200

    Created courses route and controller, all tested in postman

---
+++ controllers/courses.js
@@ -0,0 +1,7 @@
+// Create a GET route
+const get = (req, res) => {
+    res.send("The Courses I am taking this semester are Intro App Dev, UX Engineering,Databases 2 and Studio 4");
+};
+
+// Export the get function
+export { get };
--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 // Import the Express module
 import express from 'express';
 
+// Import the index routes module
+import indexRoutes from './routes/index.js';
+import aboutRoutes from './routes/about.js';
+import contactRoutes from './routes/contact.js';
+import coursesRoutes from './routes/courses.js';
+
+
 // Create an Express application
 const app = express();
 
-// Create a GET route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+// Use the routes module
+app.use('/', indexRoutes);
+app.use('/about', aboutRoutes);
+app.use('/contact',contactRoutes);
+app.use('/courses',coursesRoutes);
+
 
 // Start the server on port 3000
 app.listen(3000, () => {
   console.log('Server is listening on port 3000.');
 });
 
-// Export the Express application. May be used by other modules. For example, API testing
+// Export the Express application. Other modules may use it. For example, API testing
 export default app;
