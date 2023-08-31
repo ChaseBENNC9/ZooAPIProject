020e2fb0c829419f177e05fcfc73541b407623e3
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-08-31 20:07:29 +1200

    Added Hard Coded Routes to Index Controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,20 @@
+
+const routes = [
+  '/api/v1/zoos',
+  '/api/v1/enclosures',
+  '/api/v1/animals',
+  '/api/v1/workers',
+  '/api/v1/visitors'
+];
+const routeList = routes.map(route => `<li><a href="${route}">${route}</a></li>`).join('');
+
+const html = `
+  <h1>Available Routes:</h1>
+  <ul>${routeList}</ul>
+`;
 // Create a GET route
 const get = (req, res) => {
-  res.send("Zoo");
+  res.send(html);
 };
 
 // Export the get function
