5b104f777b3fdd9a529c5e5848d2b761c82698b1
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-09-29 13:50:38 +1300

    feat: added CRUD tests for zoo table

---
+++ test/zoo.test.js
@@ -0,0 +1,96 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import { describe, it } from "mocha";
+
+import app from "../index.js";
+
+chai.use(chaiHttp);
+
+const Zoo = {
+  name: "Zoo of Otago",
+  city: "Dunedin",
+  country: "New Zealand",
+  established: "2021-01-01T00:00:00.000Z",
+};
+
+describe("Zoos", () => {
+  it("should create Zoo", (done) => {
+    chai
+      .request(app)
+      .post("/api/v1/zoos")
+      .send(Zoo)
+      .end((req, res) => {
+
+        chai.expect(res.status).to.be.equal(201);
+        chai.expect(res.body).to.be.a("object");
+        chai
+          .expect(res.body.msg)
+          .to.be.equal("zoo successfully created");
+        done();
+      });
+  });
+
+  it("should get all Zoos", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/zoos")
+      .end((req, res) => {
+
+        chai.expect(res.status).to.be.equal(200);
+        chai.expect(res.body).to.be.a("object");
+        chai.expect(res.body.data).to.be.a("array");
+        done();
+      });
+  });
+
+  it("should get zoo by id", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/zoos/1")
+      .end((req, res) => {
+
+        chai.expect(res.status).to.be.equal(200);
+        chai.expect(res.body).to.be.a("object");
+        chai.expect(res.body.data).to.be.a("object");
+        done();
+      });
+  });
+
+  it("should update Zoo by id", (done) => {
+    chai
+      .request(app)
+      .put("/api/v1/zoos/1")
+      .send(
+        {
+            name: "Zoo of Pavlova",
+            city: "Melbourne",
+            country: "Australia",
+            established: "2023-01-01T00:00:00.000Z",
+        }
+      )
+      .end((req, res) => {
+
+        chai.expect(res.status).to.be.equal(200);
+        chai.expect(res.body).to.be.a("object");
+        chai
+          .expect(res.body.msg)
+          .to.be.equal("zoo with the id: 1 successfully updated");
+        done();
+      });
+  });
+
+  it("should delete Zoo by id", (done) => {
+    chai
+      .request(app)
+      .delete("/api/v1/zoos/1")
+      .end((req, res) => {
+
+        chai.expect(res.status).to.be.equal(200);
+        chai.expect(res.body).to.be.a("object");
+        chai
+          .expect(res.body.msg)
+          .to.be.equal("zoo with the id: 1 successfully deleted");
+        done();
+      });
+  });
+});
