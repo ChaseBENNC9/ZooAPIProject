499ddacca554b9208bfbbd29c478f354ae9bf57d
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-10-05 11:13:28 +1300

    feat: beginning seed data created for enclosures

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import zooSeed from "../data/zooSeed.json" assert { type: "json" };
 import visitorSeed from "../data/visitorSeed.json" assert { type: "json" }; 
+import enclosureSeed from "../data/enclosureSeed.json" assert { type: "json" };
 import { json } from "express";
 const prisma = new PrismaClient();
 
@@ -9,12 +10,19 @@ const main = async () => {
     const deleteZoos = await prisma.zoo.deleteMany({});
     const deleteVisitors = await prisma.visitor.deleteMany({});
     const deleteAnimals = await prisma.animal.deleteMany({});
+    const deleteTourGroups = await prisma.tourGroup.deleteMany({});
+    const deleteWorkers = await prisma.worker.deleteMany({});
+    const deleteEnclosures = await prisma.enclosure.deleteMany({});
     await prisma.zoo.createMany({
       data: zooSeed,
     });
     await prisma.visitor.createMany({
       data: visitorSeed,
     });
+    await prisma.enclosure.createMany({
+      data: enclosureSeed,
+    });
+
     console.log("Database successfully seeded");
 
     await prisma.$disconnect(); // Disconnect from the database
