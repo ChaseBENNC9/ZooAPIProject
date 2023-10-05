9e6bc04f2b8cc7088400b8b4e12ef01a1de354c3
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-10-06 12:55:59 +1300

    test: separated status codes in animal tests and added sorting

--- test/animal.test.js
+++ test/animal.test.js
@@ -15,18 +15,33 @@ const Animal = {
 };
 
 describe("Animals", () => {
+
+
+    it("should sort animals descending by id", (done) => {
+        chai
+            .request(app)
+            .get("/api/v1/animals?sortBy=id&sortOrder=desc")
+            .end((req, res) => {
+                chai.expect(res.body.data).to.be.a("array");
+                console.log(res.body.data.length);
+                chai.expect(res.body.data[0].id).to.be.equal(res.body.data.length);
+                done();
+            });
+    });
+
+    
     it("should create Animal", (done) => {
         chai
             .request(app)
             .post("/api/v1/animals")
             .send(Animal)
             .end((req, res) => {
-                chai.expect(res.status).to.be.equal(201);
                 chai.expect(res.body).to.be.a("object");
                 chai.expect(res.body.msg).to.be.equal("Animal successfully created");
                 done();
             });
     });
+
 
     it("should get all Animals", (done) => {
         chai
@@ -82,4 +97,57 @@ describe("Animals", () => {
                 done();
             });
     });
+
+
+    it("should verify Create Status Code", (done) => {
+        chai
+            .request(app)
+            .post("/api/v1/animals")
+            .send(Animal)
+            .end((req, res) => {
+                chai.expect(res.status).to.be.equal(201);
+                done();
+            });
+    
+    });
+    it("should verify Get All Status Code", (done) => {
+        chai
+            .request(app)
+            .get("/api/v1/animals")
+            .end((req, res) => {
+                chai.expect(res.status).to.be.equal(200);
+                done();
+            });
+    });
+    it("should verify Get by ID Status Code", (done) => {
+        chai
+            .request(app)
+            .get("/api/v1/animals/2")
+            .end((req, res) => {
+                chai.expect(res.status).to.be.equal(200);
+                done();
+            });
+    });
+    it("should verify Update Status Code", (done) => {
+        chai
+            .request(app)
+            .put("/api/v1/animals/2")
+            .send({
+                deathDate: "2023-05-10T00:00:00.000Z",
+            })
+            .end((req, res) => {
+                chai.expect(res.status).to.be.equal(200);
+                done();
+            });
+    });
+    it("should verify Delete Status Code", (done) => {
+        chai
+            .request(app)
+            .delete("/api/v1/animals/2")
+            .end((req, res) => {
+                chai.expect(res.status).to.be.equal(200);
+                done();
+            });
+    });
+
 });
