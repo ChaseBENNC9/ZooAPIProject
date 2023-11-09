2817ba1fa4c6575a5b26c88c60857dcbabeaf94a
ChaseBENNC9 <bennc9@student.op.ac.nz>
2023-11-10 09:53:37 +1300

    refactor: removed unused code from App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,3 @@
-//1
-// The useEffect hook is used to perform side effects in a functional component.
-//2
-/**
- * const Example = (name,school,age) => {
- * return (
- * <div>
- * <h1>My name is {name}</h1>
- * <h2>I am {age} years old</h2>
- * <h3>I am a student at {school}</h3>
- * </div>
- * )
- */
 import Navigation from "./components/Navigation";
 const App = () => {
   return (
